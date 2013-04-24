f32cf841cb26282088ab36e425157022250905d9
wehu <huwei04@hotmail.com>
2013-04-24 12:41:06 +0800

    refine printer 	modified:   lib/CljPerl/Printer.pm

--- lib/CljPerl/Printer.pm
+++ lib/CljPerl/Printer.pm
@@ -9,11 +9,24 @@ package CljPerl::Printer;
     my $type = $obj->type();
     my $s = "";
     if($class eq "Seq") {
-      $s = "(";
+      if($type eq "vector") {
+        $s = "[";
+      } elsif(($type eq "map")) {
+        $s = "{";
+      } else {
+        $s = "(";
+      }
       foreach my $i (@{$obj->value()}) {
         $s .= to_string($i) . " ";
       }
-      $s .= ")";
+      if($type eq "vector") {
+        $s .= "]";
+      } elsif(($type eq "map")) {
+        $s .= "}";
+      } else {
+        $s .= ")";
+      }
+      $s =~ s/ ([\)\]\}])$/$1/;
     } else {
       if($type eq "vector") {
         $s = "[";
@@ -21,12 +34,14 @@ package CljPerl::Printer;
           $s .= to_string($i) . " ";
         }
         $s .= "]";
+        $s =~ s/ \]$/\]/;
       } elsif($type eq "map" or $type eq "meta") {
         $s = "{";
         foreach my $i (keys %{$obj->value()}) {
           $s .= $i . "=>" . to_string($obj->value()->{$i}) . " ";
         }
         $s .= "}";
+        $s =~ s/ \}$/\}/;
       } elsif($type eq "function" or $type eq "macro") {
         $s = to_string($obj->value());
       } else {
